39097ecb293ed663dc3060111c10039efe667ad3
ehasun15019 <ehasunulislam0202@gmail.com>
2025-09-25 20:54:26 +0600

    completed ProgressResolve.jsx and create json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
+import ProgressResolve from './Components/ProgressResolve'
 
 const App = () => {
   return (
     <div className=''>
       <Navbar/>
+
+      <div className='px-12'>
+        <ProgressResolve/>
+      </div>
+
       <Footer/>
     </div>
   )
